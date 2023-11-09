68d15094fb62cb38a45b226b1b469324e0ddc3db
vanessabrewer <146860453+vanessabrewer@users.noreply.github.com>
2023-11-09 15:08:18 -0400

    Add files via upload

---
+++ prog1700_w0487885_dictionary_exercise.py
@@ -0,0 +1,19 @@
+# empty dictionary
+student_scores = {
+    "Alice": 90,
+    "Kane": 85,
+    "Charlie": 78,
+    "Vincent": 92,
+    "Eve": 88,
+}
+
+# add names and scores to dictionary
+thisdict = { 
+    "Alice": 90,
+    "Kane": 85,
+    "Charlie": 78,
+    "Vincent": 92,
+    "Eve": 88,
+}
+print(thisdict)
+
